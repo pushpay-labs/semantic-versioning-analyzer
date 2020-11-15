a8382c63c104160f3eaaa045be4de6bd49ff9655
Greg Dennis <gregsdennis@yahoo.com>
2020-11-16 10:38:20 +1300

    added documentation test to ensure readme contains all built-in rules

---
+++ tests/SemVerAnalyzer.Tests/DocumentationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using PowerAssert;
+using Xunit;
+
+namespace Pushpay.SemVerAnalyzer.Tests
+{
+	public class DocumentationTests
+	{
+		[Fact]
+		public void ReadmeListsAllBuiltInRules()
+		{
+			var readmeLines = File.ReadAllLines("README.md");
+			var ruleInterfaceType = typeof(AppModule).Assembly
+				.GetTypes()
+				.FirstOrDefault(t => t.Name == "IVersionAnalysisRule" && !t.IsGenericType);
+			var ruleTypes = typeof(AppModule).Assembly
+				.GetTypes()
+				.Where(t => !t.IsAbstract && !t.IsInterface && ruleInterfaceType.IsAssignableFrom(t));
+
+			var sb = new StringBuilder();
+
+			foreach (Type type in ruleTypes)
+			{
+				if (!readmeLines.Contains($"  - `{type.Name}`"))
+				{
+					sb.AppendLine(type.Name);
+				}
+			}
+
+			PAssert.IsTrue(() => sb.ToString() == string.Empty);
+		}
+	}
+}

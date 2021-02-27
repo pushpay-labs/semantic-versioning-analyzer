0a656ee189c2773ccc28b5e2967e651e24f8c6e2
Greg Dennis <gregsdennis@yahoo.com>
2021-02-27 23:52:53 +1300

    create partial integration tests to support specific scenarios easily

---
+++ tests/SemVerAnalyzer.Tests/MethodCheckTests.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace Pushpay.SemVerAnalyzer.Tests
+{
+	public class MethodCheckTests : StandardRuleCheckBase
+	{
+		class Local : Base
+		{
+			public override void VirtualMethod() { }
+		}
+
+		class Reference : Base
+		{
+		}
+
+		abstract class Base
+		{
+			public virtual void VirtualMethod() { }
+		}
+
+		public MethodCheckTests(IntegrationFixture integrationFixture)
+			: base(integrationFixture)
+		{
+		}
+
+		[Fact]
+		public void Test()
+		{
+			PerformFieldCheck<Reference, Local>();
+		}
+	}
+}
